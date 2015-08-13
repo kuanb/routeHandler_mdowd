e8830c3fd1c02b0aea3da927b09445eadc6038fa
Kuan Butts <kuanbutts@yahoo.com>
2015-08-13 16:53:12 -0400

    checker against fails and blocks from the googs

--- revised.py
+++ revised.py
@@ -1,11 +1,12 @@
 from urllib2 import urlopen
+import requests
 import json
 import datetime
 import time
 import pandas as pd
 
 
-zoneTable = pd.read_csv(r"fullRoutes.csv")
+zoneTable = pd.read_csv(r"short.csv")
 
 urlDict = {}
 outputDict = {}
@@ -37,60 +38,66 @@ for row in zoneTable.iterrows():
 for key, route in urlDict.iteritems():
     
     # this method returns Google LiteMaps query result (no info about browser is why)
-    response = urlopen(route).read()
+    # response = urlopen(route).read()
+    response = requests.get(route).text
     
     # risky (esp 'end') since you are assuming that you'll get that common phrase somewhere else, would make more "specific"
     start = response.find('cacheResponse(') + len('cacheResponse(')
     end = response.find(']);')+1 
 
-    cleaned = json.loads(response[start:end])
-    output = cleaned[10][0][0][0]
-    
-    currTime = str(datetime.datetime.now())
-    travelInfo = [key[0], key[1], key[2], currTime, output[0], output[1][1], output[6][0][1]]
-    
     try:
-        travelInfo.append(output[6][4][2])
-    except IndexError:
-        travelInfo.append('NA')
+        cleaned = json.loads(response[start:end])
+        output = cleaned[10][0][0][0]
+        
+        currTime = str(datetime.datetime.now())
+        travelInfo = [key[0], key[1], key[2], currTime, output[0], output[1][1], output[6][0][1]]
+        
+        try:
+            travelInfo.append(output[6][4][2])
+        except IndexError:
+            travelInfo.append('NA')
 
-    travelInfo.append(route)        
+        travelInfo.append(route)        
 
-    outputDict[key] = travelInfo
-    rawOutputDict[key] = output
+        outputDict[key] = travelInfo
+        rawOutputDict[key] = output
 
-    try:
-        sanityDict[key] = output[1][10][0][1][0]
-        sanityList.append(key)
-    except (IndexError, TypeError) as e:
-        sanityDict[key] = ''
+        try:
+            sanityDict[key] = output[1][10][0][1][0]
+            sanityList.append(key)
+        except (IndexError, TypeError) as e:
+            sanityDict[key] = ''
 
-
-
-toPandas = [i for i in outputDict.values()]
-headers = ['Origin', 'Destination', 'ShortName', 'TimeRetr', 'Route', 'Dist', 'CurrentTime', 'TimeRange','url']
-travelData = pd.DataFrame(toPandas, columns= headers)
+    except ValueError:
+        failed = True
+        print 'Failed to get correct HTML response from Google.'
 
 
-
-def produceTime(val):
-    outVal = val
-    if 'h' in val:
-        outVal = val.split(' ')
-        if len(outVal) > 2:
-            outVal = 60 * int(outVal[0]) + int(outVal[2])
-        else:
-            outVal = 60 * int(outVal[0])
-    elif 'min' in val:
-        outVal = int(val.replace('min', ''))
-    return outVal
+if not failed:
+    toPandas = [i for i in outputDict.values()]
+    headers = ['Origin', 'Destination', 'ShortName', 'TimeRetr', 'Route', 'Dist', 'CurrentTime', 'TimeRange','url']
+    travelData = pd.DataFrame(toPandas, columns= headers)
 
 
 
-travelData['nDist'] = (travelData.Dist.apply(lambda x: x[0:4])).astype(float)
-travelData['nTime'] = (travelData.CurrentTime.apply(lambda x: produceTime(x)))
+    def produceTime(val):
+        outVal = val
+        if 'h' in val:
+            outVal = val.split(' ')
+            if len(outVal) > 2:
+                outVal = 60 * int(outVal[0]) + int(outVal[2])
+            else:
+                outVal = 60 * int(outVal[0])
+        elif 'min' in val:
+            outVal = int(val.replace('min', ''))
+        return outVal
 
-travelData = travelData.sort(["Origin", "Destination"])
 
 
-travelData.to_csv('output.csv', sep=',', encoding='utf-8')
+    travelData['nDist'] = (travelData.Dist.apply(lambda x: x[0:4])).astype(float)
+    travelData['nTime'] = (travelData.CurrentTime.apply(lambda x: produceTime(x)))
+
+    travelData = travelData.sort(["Origin", "Destination"])
+
+
+    travelData.to_csv('output.csv', sep=',', encoding='utf-8')
